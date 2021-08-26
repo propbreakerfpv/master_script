8658861005c64a69e4abee943249aef71a84e315
propbreakerfpv <jonasebreen@gmail.com>
2021-08-26 01:23:48 -0400

    0.1.1.1

--- src/parcer/parce.c
+++ src/parcer/parce.c
@@ -2,8 +2,20 @@
 
 Token** parce(char* code){
   int i = 0;
+  Line line;
+  line.file = "main.ms";
+  Section sec;
+  sec.line = 0;
+  sec.row = 0;
+  sec.file = "main.ms";
+  Token token;
+  token.file = "main,ms";
+  token.line = 0;
+  token.row = 0;
   while (code[i] != '\0') {
     printf("%c\n", code[i]);
+    if(code[i] == ' '){
+    }
     i++;
   }
 }
